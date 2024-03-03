463bea0010aa855e09bcd3160186a55ba41f4935
AliHMohammad <alihmohammad@hotmail.com>
2024-03-03 18:37:10 +0100

    updated options-method when fetching

--- src/services/fetchUtils.ts
+++ src/services/fetchUtils.ts
@@ -1,21 +1,32 @@
+
+
+
 /**
  * Utility Method to create options for a fetch call
  * @param method GET, POST, PUT, DELETE
  * @param body  The request body (only relevant for POST and PUT)
- * @returns 
+ * @param authRequired
+ * @returns
  */
-export function makeOptions(method: string, body: object | null): RequestInit {
+export function makeOptions(method: string, body: object | null, authRequired?: boolean): RequestInit {
+  const headers = new Headers();
+  headers.append("Content-type", "application/json");
+  headers.append("Accept", "application/json");
+
+  if (authRequired) {
+    const token = localStorage.getItem("token")
+    headers.append("Authorization", `Bearer ${token}`);
+  }
+
   const opts: RequestInit = {
     method: method,
-    headers: {
-      "Content-type": "application/json",
-      Accept: "application/json",
-      "Authorization": "Bearer " + localStorage.getItem("token") || ""
-    },
+    headers: headers
   };
+
   if (body) {
     opts.body = JSON.stringify(body);
   }
+
   return opts;
 }
 
